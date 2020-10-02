6c0485a5f645b951f37d74407fe251445b0c2f22
ParishBen <ParishBen13@gmail.com>
2020-10-02 00:04:50 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,12 +2,12 @@ class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
 
-
+  
+#change made
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
       category = Category.find_or_create_by(category_attribute)
       self.post_categories.build(category: category)
     end
   end
-
 end
